018a74f11b9df5f39285fabdfd5f8963e3bb7d4b
Umar Bisharat <muhammadumarjatt303@gmail.com>
2026-01-23 12:54:11 +0500

    Create 12.Chapter 4 part 3.dart

---
+++ 12.Chapter 4 part 3.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main(){
+ runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Practice App',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 40,color: Colors.white),)),
+          backgroundColor: Colors.pinkAccent,
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    height: 200,
+                    width: 200,
+                    color: Colors.red,
+                  ),
+                  Positioned(
+                    right: 0,
+                    child: Container(
+                      height: 180,
+                      width: 180,
+                      color: Colors.green,
+                    ),
+                  ),
+                  Positioned (
+                    top: 10,
+                    child:  Text('Umar'),
+                  ),
+
+                ],
+              ),
+            ),
+            Stack(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 100,
+                  width: 50,
+                  color: Colors.purpleAccent,
+                ),
+                Container(
+                  height: 100,
+                  width: 10,
+                  color: Colors.green,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
